27091a1699212c495d5633e00a5a764c6a9cb3df
Scorpio7736 <jack@thelondons.com>
2026-01-10 02:49:22 -0600

    feat: Expand map and game data enums
    Expands the `MapNames` enum to include a comprehensive list of maps for Black Ops III, Black Ops IV, and Black Ops Cold War.
    
    The `games` enum is updated to include `Black_Ops_IV`.

--- app/src/main/java/com/example/the_ultimate_easter_egg_guide/MapNames.java
+++ app/src/main/java/com/example/the_ultimate_easter_egg_guide/MapNames.java
@@ -25,11 +25,40 @@ public enum MapNames
     BOII_Die_Rise(games.Black_Ops_II, "Die Rise"),
     BOII_Mob_Of_The_Dead(games.Black_Ops_II, "Mob of the Dead"),
     BOII_Buried(games.Black_Ops_II, "Buried"),
-    BOII_Origins(games.Black_Ops_II, "Origins");
+    BOII_Origins(games.Black_Ops_II, "Origins"),
 
     // --------------------------------- Black Ops III MAPS ---------------------------------
+
+    BOIII_Shadows_Of_Evil(games.Black_Ops_III, "Shadows of Evil"),
+    BOIII_The_Giant(games.Black_Ops_III, "The Giant"),
+    BOIII_Der_Eisendrache(games.Black_Ops_III, "Der Eisendrache"),
+    BOIII_Zetsubou_No_Shima(games.Black_Ops_III, "Zetsubou No Shima"),
+    BOIII_Gorod_Krovi(games.Black_Ops_III, "Gorod Krovi"),
+    BOIII_Revelations(games.Black_Ops_III, "Revelations"),
+    BOIII_Nacht_Der_Untoten(games.Black_Ops_III, "Nacht der Untoten"),
+    BOIII_Verruckt(games.Black_Ops_III, "Verruckt"),
+    BOIII_Shi_No_Numa(games.Black_Ops_III, "Shi No Numa"),
+    BOIII_Kino_Der_Toten(games.Black_Ops_III, "Kino der toten"),
+    BOIII_Ascension(games.Black_Ops_III, "Ascension"),
+    BOIII_Shangri_La(games.Black_Ops_III, "Shangri La"),
+    BOIII_Moon(games.Black_Ops_III, "Moon"),
+    BOIII_Origins(games.Black_Ops_III, "Origins"),
+
+
     // --------------------------------- Black Ops IV MAPS ---------------------------------
+
+    BOIv_Dead_Of_The_Night(games.Black_Ops_IV, "Dead of the Night"),
+    BOIV_Ancient_Evil(games.Black_Ops_IV, "Ancient Evil"),
+    BOIV_Alpha_Omega(games.Black_Ops_IV, "Alpha Omega"),
+    BOIV_Tag_Der_Toten(games.Black_Ops_IV, "Tag der toten"),
+
     // --------------------------------- Black Ops Cold War MAPS ---------------------------------
+
+    BOCW_Die_Maschine(games.Black_Ops_Cold_War, "Die Maschine"),
+    BOCW_Firebase_Z(games.Black_Ops_Cold_War, "Firebase Z"),
+    BOCW_Mauer_Der_Toten(games.Black_Ops_Cold_War, "Mauer der toten"),
+    BOCW_Forsaken(games.Black_Ops_Cold_War, "Forsaken");
+
     // --------------------------------- Black Ops VI MAPS ---------------------------------
     // --------------------------------- Black Ops VII MAPS ---------------------------------
 
--- app/src/main/java/com/example/the_ultimate_easter_egg_guide/games.java
+++ app/src/main/java/com/example/the_ultimate_easter_egg_guide/games.java
@@ -6,6 +6,7 @@ public enum games
     Black_Ops_I,
     Black_Ops_II,
     Black_Ops_III,
+    Black_Ops_IV,
     Black_Ops_Cold_War,
     Black_Ops_VI,
     Black_Ops_VII
