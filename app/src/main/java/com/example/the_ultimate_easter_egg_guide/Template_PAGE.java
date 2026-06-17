3edd1bebe35b6248a2df129c9ea62a4c9f7519ea
Scorpio7736 <jack@thelondons.com>
2026-06-16 23:17:05 -0500

    added removed files. IDK why this happened but did not push to git. This will be the first push to work i guess.

---
+++ app/src/main/java/com/example/the_ultimate_easter_egg_guide/Template_PAGE.java
@@ -0,0 +1,32 @@
+package com.example.the_ultimate_easter_egg_guide;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Template_PAGE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.template_page);
+    }
+
+    public void onHomeButtonClick(View view) {
+        startActivity(new Intent(this, Home_PAGE.class));
+    }
+
+    public void onMapsButtonClick(View view) {
+        startActivity(new Intent(this, MapsSelection_Page.class));
+    }
+
+    public void onSettingsButtonClick(View view) {
+        // Replace Settings_PAGE.class with your actual settings activity name.
+    }
+
+    public void onTemplateButtonClick(View view) {
+        // Already on the template page.
+    }
+}
