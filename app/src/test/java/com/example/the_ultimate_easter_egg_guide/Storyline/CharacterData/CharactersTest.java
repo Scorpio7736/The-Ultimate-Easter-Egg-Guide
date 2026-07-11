5962228d0281b7867b59c0e0b07eea81e50f7318
Scorpio7736 <jack@thelondons.com>
2026-07-11 01:24:35 -0500

    Made unit tests for new items. 82% CC!

---
+++ app/src/test/java/com/example/the_ultimate_easter_egg_guide/Storyline/CharacterData/CharactersTest.java
@@ -0,0 +1,51 @@
+package com.example.the_ultimate_easter_egg_guide.Storyline.CharacterData;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.example.the_ultimate_easter_egg_guide.Models.Storyline.CharacterGroup;
+import com.example.the_ultimate_easter_egg_guide.Models.Storyline.CharacterVersion;
+
+import org.junit.Test;
+
+public class CharactersTest {
+
+    @Test
+    public void testPlayerCharactersNotNull() {
+        for (Player_Characters character : Player_Characters.values()) {
+            assertNotNull("Character name should not be null for " + character.name(), character.characterName);
+            assertNotNull("Character group should not be null for " + character.name(), character.characterGroup);
+            assertNotNull("Character version should not be null for " + character.name(), character.characterVersion);
+            assertNotNull("Nicknames list should not be null for " + character.name(), character.characterNicknames);
+            assertTrue("Character image should be valid for " + character.name(), character.characterImage != 0);
+        }
+    }
+
+    @Test
+    public void testNonPlayerCharactersNotNull() {
+        for (NonPlayer_Characters character : NonPlayer_Characters.values()) {
+            assertNotNull("Character name should not be null for " + character.name(), character.characterName);
+            assertNotNull("Character group should not be null for " + character.name(), character.characterGroup);
+            assertNotNull("Character version should not be null for " + character.name(), character.characterVersion);
+            assertNotNull("Nicknames list should not be null for " + character.name(), character.characterNicknames);
+            assertTrue("Character image should be valid for " + character.name(), character.characterImage != 0);
+        }
+    }
+
+    @Test
+    public void testSpecificPlayerCharacter() {
+        Player_Characters richtofen = Player_Characters.EdwardRichtofen_Primis;
+        assertEquals("Dr. Edward Richtofen", richtofen.characterName);
+        assertEquals(CharacterGroup.Primis, richtofen.characterGroup);
+        assertEquals(CharacterVersion.Primis, richtofen.characterVersion);
+    }
+
+    @Test
+    public void testSpecificNonPlayerCharacter() {
+        NonPlayer_Characters maxis = NonPlayer_Characters.LudwigMaxis_Ultimis;
+        assertEquals("Dr. Ludwig Maxis", maxis.characterName);
+        assertEquals(CharacterGroup.Group935_Ultimis, maxis.characterGroup);
+        assertEquals(CharacterVersion.Ultimis, maxis.characterVersion);
+    }
+}
