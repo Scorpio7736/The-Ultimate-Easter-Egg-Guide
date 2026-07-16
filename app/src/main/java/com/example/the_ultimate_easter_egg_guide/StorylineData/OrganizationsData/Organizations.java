c70222484208039ef47e59e063786202043039d5
Scorpio7736 <jack@thelondons.com>
2026-07-15 23:28:46 -0500

    Added organization profile pictures and updated the Organizations enum.

--- app/src/main/java/com/example/the_ultimate_easter_egg_guide/StorylineData/OrganizationsData/Organizations.java
+++ app/src/main/java/com/example/the_ultimate_easter_egg_guide/StorylineData/OrganizationsData/Organizations.java
@@ -12,19 +12,19 @@ import java.util.List;
 public enum Organizations implements IStorylineItems
 {
     TEST("Test Organization", R.drawable.app_icon, OrganizationGroups.TEST, Collections.singletonList(Games.Test), "Category:Organizations"),
-    Group935("Group 935", R.drawable.app_icon, OrganizationGroups.German, Games.GetAllGamesBetween(Games.World_At_War, Games.Black_Ops_IV), "Group_935"),
-    Division9("Division 9", R.drawable.app_icon, OrganizationGroups.Japanese, Collections.singletonList(Games.Black_Ops_III), "Division_9"),
-    Ascension("The Ascension Group", R.drawable.app_icon, OrganizationGroups.Soviet, Arrays.asList(Games.Black_Ops_I, Games.Black_Ops_III), "Ascension_Group"),
-    BrokenArrow("Broken Arrow", R.drawable.app_icon, OrganizationGroups.American, Arrays.asList(Games.Black_Ops_II, Games.Black_Ops_IV), "Broken_Arrow"),
-    USGovernment("US Government", R.drawable.app_icon, OrganizationGroups.American, Games.GetAllGamesBetween(Games.World_At_War, Games.Black_Ops_IV), "United_States_Government"),
-    DepartmentOfDefence("Department of Defense", R.drawable.app_icon, OrganizationGroups.American, Arrays.asList(Games.Black_Ops_I, Games.Black_Ops_IV), "Department_of_Defense"),
-    CIA("C.I.A.", R.drawable.app_icon, OrganizationGroups.American, Collections.singletonList(Games.Black_Ops_II), "Central_Intelligence_Agency"),
-    CDC("C.D.C.", R.drawable.app_icon, OrganizationGroups.American, Collections.singletonList(Games.Black_Ops_II), "Centers_for_Disease_Control_and_Prevention"),
-    SovietUnion("Soviet Union", R.drawable.app_icon, OrganizationGroups.Soviet, Arrays.asList(Games.Black_Ops_I, Games.Black_Ops_III), "Soviet_Union"),
-    TheKeepers("The Keepers", R.drawable.app_icon, OrganizationGroups.Extraterrestrial, Arrays.asList(Games.Black_Ops_III, Games.Black_Ops_IV), "Keeper"),
-    TheApothicons("The Apothicons", R.drawable.app_icon, OrganizationGroups.Extraterrestrial, Arrays.asList(Games.Black_Ops_III, Games.Black_Ops_IV), "Apothicon"),
-    Requiem("Requiem", R.drawable.app_icon, OrganizationGroups.American, Collections.singletonList(Games.Black_Ops_Cold_War), "Office_of_Requiem"),
-    OmegaGroup("Omega Group", R.drawable.app_icon, OrganizationGroups.Soviet, Collections.singletonList(Games.Black_Ops_Cold_War), "Omega_Group");
+    Group935("Group 935", R.drawable.group935_pfp, OrganizationGroups.German, Games.GetAllGamesBetween(Games.World_At_War, Games.Black_Ops_IV), "Group_935"),
+    Division9("Division 9", R.drawable.division9_bkg, OrganizationGroups.Japanese, Collections.singletonList(Games.Black_Ops_III), "Division_9"),
+    Ascension("The Ascension Group", R.drawable.ascensiongroup_pfp, OrganizationGroups.Soviet, Arrays.asList(Games.Black_Ops_I, Games.Black_Ops_III), "Ascension_Group"),
+    BrokenArrow("Broken Arrow", R.drawable.brokenarrow_pfp, OrganizationGroups.American, Arrays.asList(Games.Black_Ops_II, Games.Black_Ops_IV), "Broken_Arrow"),
+    USGovernment("US Government", R.drawable.usgovernment_pfp, OrganizationGroups.American, Games.GetAllGamesBetween(Games.World_At_War, Games.Black_Ops_IV), "United_States_Government"),
+    DepartmentOfDefence("Department of Defense", R.drawable.departmentofdefense_pfp, OrganizationGroups.American, Arrays.asList(Games.Black_Ops_I, Games.Black_Ops_IV), "Department_of_Defense"),
+    CIA("C.I.A.", R.drawable.cia_pfp, OrganizationGroups.American, Collections.singletonList(Games.Black_Ops_II), "Central_Intelligence_Agency"),
+    CDC("C.D.C.", R.drawable.cdc_pfp, OrganizationGroups.American, Collections.singletonList(Games.Black_Ops_II), "Centers_for_Disease_Control_and_Prevention"),
+    SovietUnion("Soviet Union", R.drawable.ussr_pfp, OrganizationGroups.Soviet, Arrays.asList(Games.Black_Ops_I, Games.Black_Ops_III), "Soviet_Union"),
+    TheKeepers("The Keepers", R.drawable.keepers_pfp, OrganizationGroups.Extraterrestrial, Arrays.asList(Games.Black_Ops_III, Games.Black_Ops_IV), "Keeper"),
+    TheApothicons("The Apothicons", R.drawable.apothicon_pfp, OrganizationGroups.Extraterrestrial, Arrays.asList(Games.Black_Ops_III, Games.Black_Ops_IV), "Apothicon"),
+    Requiem("Requiem", R.drawable.requiem_pfp, OrganizationGroups.American, Collections.singletonList(Games.Black_Ops_Cold_War), "Office_of_Requiem"),
+    OmegaGroup("Omega Group", R.drawable.omegagroup_pfp, OrganizationGroups.Soviet, Collections.singletonList(Games.Black_Ops_Cold_War), "Omega_Group");
 
 
     public final String displayName;
