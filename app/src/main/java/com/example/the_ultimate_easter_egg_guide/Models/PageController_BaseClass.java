7c243960f321d40ed787f9152ae598f07508706a
Scorpio7736 <jack@thelondons.com>
2026-07-17 18:52:36 -0500

    need pro so testing will not work.

--- app/src/main/java/com/example/the_ultimate_easter_egg_guide/Models/PageController_BaseClass.java
+++ app/src/main/java/com/example/the_ultimate_easter_egg_guide/Models/PageController_BaseClass.java
@@ -11,7 +11,7 @@ import com.example.the_ultimate_easter_egg_guide.R;
 
 public abstract class PageController_BaseClass extends AppCompatActivity {
     
-    public final boolean ENABLE_TESTING = true;
+    public final boolean ENABLE_TESTING = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
